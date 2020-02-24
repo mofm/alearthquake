0d03c669fc83d8d6c5a77f45d3bf380919ce62f7
mofm <emre.eryilmaz@piesso.com>
2020-02-25 01:35:17 +0300

    changed constants lower to uppercase(pep8)

--- alearthquake.py
+++ alearthquake.py
@@ -7,27 +7,27 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
-config = configparser.ConfigParser()
-config.read('./alearthquake1.ini')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('./alearthquake1.ini')
 
-r = requests.get('http://www.koeri.boun.edu.tr/scripts/lst0.asp')
+REQ = requests.get('http://www.koeri.boun.edu.tr/scripts/lst0.asp')
 
-c = r.content
+CON = REQ.content
 
-soup = BeautifulSoup(c, 'html.parser')
-main_content = soup.pre.get_text()
-lines = main_content.splitlines()
+SOUP = BeautifulSoup(CON, 'html.parser')
+MAIN_CONTENT = SOUP.pre.get_text()
+LINES = MAIN_CONTENT.splitlines()
 
 
 def ktimestamp():
-    for nr, i in enumerate(lines):
+    for nr, i in enumerate(LINES):
         if nr == 7:
             amma = i.split()
             date_string = '%sT%s' % ((amma[0]), (amma[1]))
             format_date = datetime.strptime(date_string, '%Y.%m.%dT%H:%M:%S')
             lastquake = format_date.timestamp()
 
-    with open(config['tracker']['tracker_file'], 'r+') as f:
+    with open(CONFIG['tracker']['tracker_file'], 'r+') as f:
         last = float(f.readline())
         if last == lastquake:
             exit()
@@ -42,14 +42,14 @@ def lastquake_line(lastquake):
     dt_convert = datetime.fromtimestamp(lastquake)
     dt_format = datetime.strftime(dt_convert, '%Y.%m.%d %H:%M:%S', )
     dt_pattern = re.compile('^%s.*' % dt_format)
-    for nr, i in enumerate(lines):
+    for nr, i in enumerate(LINES):
         if dt_pattern.search(i):
             return nr
 
 
 def bycoordinates(latitude, longitude, magnitude, lastline=506):
-    la_pattern = re.compile('^(%s.*)\s(%s)' % (latitude, longitude))
-    for nr, i in enumerate(lines):
+    la_pattern = re.compile(r'^(%s.*)\s(%s)' % (latitude, longitude))
+    for nr, i in enumerate(LINES):
         if 7 <= nr <= lastline:
             i_split = i.split()
             new_line = i_split[2] + ' ' + i_split[3]
@@ -59,7 +59,7 @@ def bycoordinates(latitude, longitude, magnitude, lastline=506):
 
 def byregion(region, magnitude, lastline=506):
     rg_pattern = re.compile('.*(%s).*' % region)
-    for nr, i in enumerate(lines):
+    for nr, i in enumerate(LINES):
         if 7 <= nr <= lastline:
             i_split = i.split()
             new_line = i_split[8] + ' ' + i_split[9]
@@ -74,10 +74,10 @@ Subject: Hi there
 This message is sent from Python."""
     context = ssl.create_default_context()
     try:
-        with smtplib.SMTP(config['mail']['smtp_server'], config['mail']['port']) as server:
+        with smtplib.SMTP(CONFIG['mail']['smtp_server'], CONFIG['mail']['port']) as server:
             server.starttls(context=context)
-            server.login(config['mail']['username'], config['mail']['password'])
-            server.sendmail(config['notification']['sender_mail'], config['notification']['receiver_mail'], message)
+            server.login(CONFIG['mail']['username'], CONFIG['mail']['password'])
+            server.sendmail(CONFIG['notification']['sender_mail'], CONFIG['notification']['receiver_mail'], message)
             print('Success')
     except Exception as e:
         print(e)
