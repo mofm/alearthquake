5d75b8c5e772315c90ae0572aa3c727284d5c409
mofm <emre.eryilmaz@piesso.com>
2020-02-25 23:32:03 +0300

    added docstrings and changed variable names(snake_case naming style)

--- alearthquake.py
+++ alearthquake.py
@@ -1,3 +1,4 @@
+""" This script sends mail notifications about last earthquakes"""
 import configparser
 import re
 import smtplib
@@ -20,37 +21,40 @@ LINES = MAIN_CONTENT.splitlines()
 
 
 def ktimestamp():
-    for nr, i in enumerate(LINES):
-        if nr == 7:
+    """check new earthquake time from KOERI and compare last checked timestamp(tracker file)."""
+    for nr1, i in enumerate(LINES):
+        if nr1 == 7:
             amma = i.split()
             date_string = '%sT%s' % ((amma[0]), (amma[1]))
             format_date = datetime.strptime(date_string, '%Y.%m.%dT%H:%M:%S')
             lastquake = format_date.timestamp()
 
-    with open(CONFIG['tracker']['tracker_file'], 'r+') as f:
-        last = float(f.readline())
+    with open(CONFIG['tracker']['tracker_file'], 'r+') as file:
+        last = float(file.readline())
         if last == lastquake:
             exit()
         else:
-            f.seek(0)
-            f.write('%s' % lastquake)
-            f.truncate()
+            file.seek(0)
+            file.write('%s' % lastquake)
+            file.truncate()
             return last
 
 
 def lastquake_line(lastquake):
+    """find previously checked last line"""
     dt_convert = datetime.fromtimestamp(lastquake)
     dt_format = datetime.strftime(dt_convert, '%Y.%m.%d %H:%M:%S', )
     dt_pattern = re.compile('^%s.*' % dt_format)
-    for nr, i in enumerate(LINES):
+    for nr2, i in enumerate(LINES):
         if dt_pattern.search(i):
-            return nr
+            return nr2
 
 
 def bycoordinates(latitude, longitude, magnitude, lastline=506):
+    """find last earthquakes by coordinates"""
     la_pattern = re.compile(r'^(%s.*)\s(%s)' % (latitude, longitude))
-    for nr, i in enumerate(LINES):
-        if 7 <= nr <= lastline:
+    for nr3, i in enumerate(LINES):
+        if 7 <= nr3 <= lastline:
             i_split = i.split()
             new_line = i_split[2] + ' ' + i_split[3]
             if la_pattern.search(new_line) and float(i_split[6]) >= magnitude:
@@ -58,9 +62,10 @@ def bycoordinates(latitude, longitude, magnitude, lastline=506):
 
 
 def byregion(region, magnitude, lastline=506):
+    """find last earthquakes by region"""
     rg_pattern = re.compile('.*(%s).*' % region)
-    for nr, i in enumerate(LINES):
-        if 7 <= nr <= lastline:
+    for nr4, i in enumerate(LINES):
+        if 7 <= nr4 <= lastline:
             i_split = i.split()
             new_line = i_split[8] + ' ' + i_split[9]
             if rg_pattern.search(new_line) and float(i_split[6]) >= magnitude:
@@ -68,6 +73,7 @@ def byregion(region, magnitude, lastline=506):
 
 
 def send_notification():
+    """mail notification function"""
     message = """\
 Subject: Hi there
 
